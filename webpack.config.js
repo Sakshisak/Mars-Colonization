a7a057ca33f39811824c6e04eb23baa76bdd998b
Sakshisak <sakshisa@iitk.ac.in>
2020-07-24 01:26:14 +0530

    Added server.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,39 @@
 var path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const extractSass = new ExtractTextPlugin('style.css');
+
 module.exports = {
 	entry: {
 		bundle: './src/index.js',
 	},
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].js',
-    publicPath: 'dist/'
-  },
+  	output: {
+	    path: path.join(__dirname, 'dist'),
+	    filename: '[name].js',
+	    publicPath: 'dist/'
+  	},
+  	devtool: "source-map",
 	module: {
 		rules: [
+		    {test: /\.(jpe?g|png|gif|svg)$/, loader: 'file-loader?name=assets/[name].[ext]&publicPath=../dist/'},
 			{ 
 				test: /\.js$/, 
 				exclude: /node_modules/, 
-				use: 'babel-loader' 
+				loader: 'babel-loader' 
 			},
 			{
-        test: /\.scss$/,
-        use: extractSass.extract({
-            use: ["css-loader", "sass-loader"]
-        })
-      }
+	            test: /\.scss$/,
+	            loader: extractSass.extract({
+	                loader: [{
+		                loader: "css-loader", options: {
+		                    sourceMap: true
+		                }
+		            }, {
+		                loader: "sass-loader", options: {
+		                    sourceMap: true
+		                }
+		            }]
+	            })
+	        }
 		]
 	},
 	plugins: [
